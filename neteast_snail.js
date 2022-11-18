b85a772f11934bd2c7a41c5ea80a5e766e7255b5
duane <53958266+xxps@users.noreply.github.com>
2022-11-18 12:59:29 +0800

    Update neteast_snail.js

--- neteast_snail.js
+++ neteast_snail.js
@@ -1,6 +1,6 @@
 [rewrite_local]
 
-^https:\/\/p\.du\.163\.com\/gain\/readtime\/info\.json url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/yywnds.js
+^https:\/\/p\.du\.163\.com\/gain\/readtime\/info\.json url script-response-body
 
 [mitm] 
 
@@ -9,6 +9,6 @@ info.json
 
 var body = $response.body;
 var obj = JSON.parse(body);
-obj.tradeEndTime = 1668747484;
+obj.tradeEndTime = 0;
 body = JSON.stringify(obj);
 $done({body})
